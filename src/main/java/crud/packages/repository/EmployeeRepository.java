d4ff63a9e15b3c9d612a23b0d666ab11405153c9
sperlik-oliver <75211235+sperlik-oliver@users.noreply.github.com>
2021-11-22 13:01:49 +0100

    Add files via upload

---
+++ src/main/java/crud/packages/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package crud.packages.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import crud.packages.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+}
