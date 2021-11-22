4906ffc201d7c864950c27c7ad0c648702aa2d70
sperlik-oliver <75211235+sperlik-oliver@users.noreply.github.com>
2021-11-22 14:18:03 +0100

    removed test

--- src/main/java/crud/packages/model/User.java
+++ src/main/java/crud/packages/model/User.java
@@ -15,25 +15,25 @@ public class User {
     private String FirstName;
     private String LastName;
     private String Email;
-    private String Password;
-    private String Street;
-    private String Postcode;
-    private String City;
-    private String Country;
+//    private String Password;
+//    private String Street;
+//    private String Postcode;
+//    private String City;
+//    private String Country;
 
     public User() {
 
     }
 
-    public User(String FirstName, String LastName, String Email, String Password, String Street, String Postcode, String City, String Country) {
+    public User(String FirstName, String LastName, String Email) {
         this.FirstName = FirstName;
         this.LastName = LastName;
         this.Email = Email;
-        this.Password = Password;
-        this.Street = Street;
-        this.Postcode = Postcode;
-        this.City = City;
-        this.Country = Country;
+//        this.Password = Password;
+//        this.Street = Street;
+//        this.Postcode = Postcode;
+//        this.City = City;
+//        this.Country = Country;
     }
 
     @Id
@@ -60,60 +60,52 @@ public class User {
     public void setLastName(String LastName) {
         this.LastName = LastName;
     }
-
-    @Column(name = "email_address", nullable = false)
-    public String getEmail() {
-        return Email;
-    }
-    public void setEmail(String Email) {
-        this.Email = Email;
-    }
-
-    @Column(name = "password", nullable = false)
-    public String getPassword() { return Password; }
-    public void setPassword(String Password) {this.Password = Password;}
-
-    @Column(name = "street", nullable = false)
-    public String getStreet() { return Street; }
-    public void setStreet(String Street) {this.Street = Street;}
-
-    @Column(name = "postcode", nullable = false)
-    public String getPostcode() {
-        return Postcode;
-    }
-    public void setPostcode(String Postcode) {
-        this.Postcode = Postcode;
-    }
-
-    @Column(name = "city", nullable = false)
-    public String getCity() {
-        return City;
-    }
-    public void setCity(String city) {
-        this.City = city;
-    }
-
-    @Column(name = "country", nullable = false)
-    public String getCountry() {
-        return Country;
-    }
-    public void setCountry(String Country) {
-        this.Country = Country;
-    }
+//
+//    @Column(name = "email_address", nullable = false)
+//    public String getEmail() {
+//        return Email;
+//    }
+//    public void setEmail(String Email) {
+//        this.Email = Email;
+//    }
+//
+//    @Column(name = "password", nullable = false)
+//    public String getPassword() { return Password; }
+//    public void setPassword(String Password) {this.Password = Password;}
+//
+//    @Column(name = "street", nullable = false)
+//    public String getStreet() { return Street; }
+//    public void setStreet(String Street) {this.Street = Street;}
+//
+//    @Column(name = "postcode", nullable = false)
+//    public String getPostcode() {
+//        return Postcode;
+//    }
+//    public void setPostcode(String Postcode) {
+//        this.Postcode = Postcode;
+//    }
+//
+//    @Column(name = "city", nullable = false)
+//    public String getCity() {
+//        return City;
+//    }
+//    public void setCity(String city) {
+//        this.City = city;
+//    }
+//
+//    @Column(name = "country", nullable = false)
+//    public String getCountry() {
+//        return Country;
+//    }
+//    public void setCountry(String Country) {
+//        this.Country = Country;
+//    }
+//
 
 
     @Override
     public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", FirstName='" + FirstName + '\'' +
-                ", LastName='" + LastName + '\'' +
-                ", Email='" + Email + '\'' +
-                ", Password='" + Password + '\'' +
-                ", Street='" + Street + '\'' +
-                ", Postcode='" + Postcode + '\'' +
-                ", City='" + City + '\'' +
-                ", Country='" + Country + '\'' +
-                '}';
+        return "User [id=" + id + ", firstName=" + FirstName + ", lastName=" + LastName + ", emailId=" + Email
+                + "]";
     }
 }
