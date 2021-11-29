440840745a3af2df85df671f1825d82930133074
sperlik-oliver <75211235+sperlik-oliver@users.noreply.github.com>
2021-11-29 17:36:49 +0100

    long???

--- src/main/java/crud/packages/controller/HallController.java
+++ src/main/java/crud/packages/controller/HallController.java
@@ -44,6 +44,7 @@ public class HallController {
 
     @PostMapping("/hall/create")
     public ResponseEntity<Hall> createHall (@Valid @RequestBody HallDTO hallDTO) throws ResourceNotFoundException {
+        System.out.println(hallDTO);
         Branch branch = branchRepository.findById((long)hallDTO.getBranchId())
                         .orElseThrow( () -> new ResourceNotFoundException("Branch not found"));
         Hall hall = new Hall();
