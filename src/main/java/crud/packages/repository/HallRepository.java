811869a7f61ce5dcaec1fdedc025680f8853f45d
sperlik-oliver <75211235+sperlik-oliver@users.noreply.github.com>
2021-11-29 16:43:39 +0100

    fix from offical docs

--- src/main/java/crud/packages/repository/HallRepository.java
+++ src/main/java/crud/packages/repository/HallRepository.java
@@ -8,6 +8,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface HallRepository extends JpaRepository<Hall, Long> {
-    @Query("SELECT b FROM Hall h JOIN Branch b ON h.branch_id = b.id WHERE h.id = :id")
-    public Branch getHallBranch(@Param("id")long hallId);
+//    @Query("SELECT b FROM Hall h JOIN Branch b ON h.branch_id = b.id WHERE h.id = :id")
+//    public Branch getHallBranch(@Param("id")long hallId);
 }
